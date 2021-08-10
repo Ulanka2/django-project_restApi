c4d85efb6442edd0f18fb3caa453341e95baf8c7
Ulan <80949499+Ulanka2@users.noreply.github.com>
2021-08-11 00:35:42 +0600

    Add tasks model

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+class Task(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='tasks', null=True)
+    body = models.TextField()
+    is_finished = models.BooleanField(default=False)
+    estimated_finish_time = models.DateTimeField()
+
