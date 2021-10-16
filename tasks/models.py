e5c12d67ff6ab76ea306a610a7f06d215e6c25e7
Ulan <80949499+Ulanka2@users.noreply.github.com>
2021-10-17 03:43:54 +0600

    Update models.py

--- tasks/models.py
+++ tasks/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth import get_user_model
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 User = get_user_model()
 
@@ -12,6 +13,25 @@ class Books(models.Model):
 
     class Meta:
         ordering = ['-id', ]
+    
+    @property
+    def rating(self):
+        ratings = self.ratings.all()
+        if ratings:
+            sum_ = 0
+            for rating in ratings:
+                sum_ += rating.value
+            return round(sum_ / len(ratings), 2)
+        return 0
+
+
+class Rating(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='ratings')
+    books = models.ForeignKey(Books, on_delete=models.CASCADE, related_name='ratings')
+    value = models.IntegerField(validators=[
+                                MaxValueValidator(5),
+                                MinValueValidator(1)
+                                ])
     
     def __str__(self):
         return f'This is your books {self.title}'
